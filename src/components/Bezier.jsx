8ad844c6c6be4cc3592bf968bdaf794779c039e5
Alexandr_Ovchar <azalexxx02@gmail.com>
2023-06-24 12:08:17 +0300

    the curve is constructed at any number of points

--- src/components/Bezier.jsx
+++ src/components/Bezier.jsx
@@ -4,6 +4,7 @@ import p5 from "p5";
 const Bezier = () => {
   const canvasRef = useRef(null);
   const points = [];
+  let selectedPoint = null;
 
   useEffect(() => {
     const sketch = (p) => {
@@ -24,20 +25,46 @@ const Bezier = () => {
         if (p.keyIsDown(p.SHIFT)) {
           const point = { x: p.mouseX, y: p.mouseY };
           points.push(point);
+        } else {
+          for (let i = 0; i < points.length; i++) {
+            const point = points[i];
+            const size = 8;
+            const halfSize = size / 2;
+            const x = point.x - halfSize;
+            const y = point.y - halfSize;
+            if (
+              p.mouseX >= x &&
+              p.mouseX <= x + size &&
+              p.mouseY >= y &&
+              p.mouseY <= y + size
+            ) {
+              selectedPoint = i;
+              break;
+            }
+          }
         }
       };
 
-      p.draw = () => {
-        // p.background(0);
-        p.stroke("red");
-        p.strokeWeight(2);
+      p.mouseDragged = () => {
+        if (selectedPoint !== null) {
+          points[selectedPoint].x = p.mouseX;
+          points[selectedPoint].y = p.mouseY;
+        }
+      };
 
-        if (points.length > 1) {
-          for (let i = 0; i < points.length - 1; i++) {
-            const p0 = points[i];
-            const p1 = points[i + 1];
-            p.line(p0.x, p0.y, p1.x, p1.y);
-          }
+      p.mouseReleased = () => {
+        selectedPoint = null;
+      };
+
+      p.draw = () => {
+        p.background(backgroundImage);
+        p.stroke("red");
+        p.strokeWeight(1);
+
+        for (let i = 0; i < points.length - 1; i++) {
+          const p0 = points[i];
+          const p1 = points[i + 1];
+          p.line(p0.x, p0.y, p1.x, p1.y);
         }
 
         p.stroke(0, 255, 0);
@@ -51,16 +78,36 @@ const Bezier = () => {
           p.rect(x, y, size, size);
         }
 
-        if (points.length >= 4) {
+        if (points.length >= 2) {
           p.stroke(255, 0, 0);
+          p.stroke("blue");
+          p.strokeWeight(2);
+
           p.noFill();
 
-          const p0 = points[0];
-          const p1 = points[1];
-          const p2 = points[2];
-          const p3 = points[3];
+          const controlPoints = [];
+          for (let i = 0; i < points.length; i++) {
+            const point = points[i];
+            controlPoints.push(point.x, point.y);
+          }
 
-          p.bezier(p0.x, p0.y, p1.x, p1.y, p2.x, p2.y, p3.x, p3.y);
+          p.beginShape();
+          p.curveVertex(controlPoints[0], controlPoints[1]);
+
+          for (let i = 0; i < controlPoints.length - 2; i += 2) {
+            p.curveVertex(
+              controlPoints[i],
+              controlPoints[i + 1],
+              controlPoints[i + 2],
+              controlPoints[i + 3]
+            );
+          }
+
+          p.curveVertex(
+            controlPoints[controlPoints.length - 2],
+            controlPoints[controlPoints.length - 1]
+          );
+          p.endShape();
         }
       };
     };
